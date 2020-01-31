e194d8a177d4ea2bef3f7f4d3798df7c1aa2a8ef
Lovisa Littbrand <lollo.littbrand@hotmail.com>
2020-01-31 15:56:08 -0800

    added updated files for login page (html, css, php) v2

--- HTML,CSS,JS/php/signin_solve.php
+++ HTML,CSS,JS/php/signin_solve.php
@@ -1,36 +1,25 @@
 <?php
 include 'db.php';
 
+//Retrieving input from form
 $user = $_POST['username'];
-$pwd = $_POST['password'];
+$pw = $_POST['password'];
 
 
-if (empty($user)) {
-    print("Username is required");
-}
-elseif (empty($pwd)) {
-    print("Password is required");
-}
-else {
-
-$user_check = "select * from DiaBeatIt.patients where DiaBeatIt.patients.username = '$user' and DiaBeatIt.patients.pwd = '$pwd'";
+//Connecting to database to see if username and password
+$user_check = "select * from DiaBeatIt.patients where DiaBeatIt.patients.username = '$user' and DiaBeatIt.patients.pwd = '$pw'";
 $result = mysqli_query($link, $user_check);
 $row = mysqli_fetch_row($result);
 
-if ($row['username' === $user]) {
-    //$url = "nutrition_form.php";
+if ($row['username' === $user] AND $row['pwd' === $pw]) {
+    //if existing user, redirecting into another page
     $url = "nutrition.html";
     header("location:".$url);
 }
 else {
-    //print("Incorrect username or password");
-    $url = "login.html";
-    header("location:".$url);
-
-     echo "<script type='text/JavaScript'>
-                alert('JavaScript is awesome!');
-            </script>";
-}
-
+    //if non-existing user, send out an alter and redirect to login-page
+    echo "<script type='text/JavaScript'>
+          alert('Incorrect username or password');
+           document.location.href = 'login.html'; </script>";
 }
 ?>
