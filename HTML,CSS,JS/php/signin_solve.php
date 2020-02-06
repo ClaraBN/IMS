d2b01201b24b200145e8538a05157cf7de0faf0c
Lovisa Littbrand <lollo.littbrand@hotmail.com>
2020-02-06 12:56:38 -0800

    updated version signin_solve

--- HTML,CSS,JS/php/signin_solve.php
+++ HTML,CSS,JS/php/signin_solve.php
@@ -7,24 +7,36 @@ $user = $_POST['username'];
 $pw = $_POST['password'];
 $_SESSION['name'] = $_POST['username'];
 
-//Connecting to database to see if username and password
-$user_check = "select * from $dbname.user where $dbname.user.username = '$user' and $dbname.user.password = '$pw'";
+//Connecting to database to see if username exists - it should be unique
+$user_check = "select * from $dbname.users where $dbname.users.username = '$user'";
 $result = mysqli_query($link, $user_check);
 $row = mysqli_fetch_row($result);
 
 
-if ($row['username' === $user] AND $row['pwd' === $pw]) {
-    //if existing user, redirecting into another page
-	$_SESSION['username']=$row[1];
-	$_SESSION['userid']=$row[0] ;
-    $url = "Home_login.php";
+if (empty($row)) {
+    //If user doesn't exist, send out an alert and redirect to login-page
+    echo "<script type='text/JavaScript'>
+          alert('Incorrect username or password!');
+           document.location.href = '../html/login.html'; </script>";
+}
+elseif (password_verify($pw, $row[3]) == TRUE AND $row[5] === 'patient') {
+    //If existing user and correct password, redirecting into another page
+    $_SESSION['username']=$row[1];
+    $_SESSION['id']=$row[0];
+    $url = "../php/Home_login.php";
+    header("location:".$url);
+}
+elseif (password_verify($pw, $row[3]) == TRUE AND $row[5] === 'doctor') {
+    //If existing user and correct password, redirecting into another page
+    $_SESSION['username']=$row[1];
+    $_SESSION['id']=$row[0];
+    $url = "../php/Home_login.php";
     header("location:".$url);
 }
 else {
-    //if non-existing user, send out an alter and redirect to login-page
-	
+    //If existing user but incorrect password, send out an alert and redirect to login-page
     echo "<script type='text/JavaScript'>
-          alert('Incorrect username or password');
-           document.location.href = '../html/login.html'; </script>";
+         alert('Incorrect username or password');
+         document.location.href = '../html/login.html'; </script>";
 }
 ?>
