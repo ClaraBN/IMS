4ef1373232c5ae3226f460c1d59b44c51555059b
Clara Bernedal Nordstroem <clara.nordstrom@live.se>
2020-02-14 15:41:38 +0100

    working version for presentation

--- HTML,CSS,JS/php/register_user.php
+++ HTML,CSS,JS/php/register_user.php
@@ -185,7 +185,7 @@ echo $total_err;
             <nav>
       <ul>
         <li><a href="../html/Home.html">Home</a></li>
-        <li><a href="..html/nutrition.html">Nutrition checker</a></li>
+        <li><a href="../html/nutrition.html">Nutrition checker</a></li>
 		 <li><a href="../html/educational_page.html">Learn more</a></li>
         <li> <a href="../html/login.html">Sign In&nbsp;</a></li>
       </ul>
