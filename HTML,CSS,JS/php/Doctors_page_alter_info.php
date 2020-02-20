c79c1a7e853859be780a1c3374a8d0182d9686a9
Lovisa Littbrand <lollo.littbrand@hotmail.com>
2020-02-20 11:57:30 -0800

    updated version doctors page

--- HTML,CSS,JS/php/Doctors_page_alter_info.php
+++ HTML,CSS,JS/php/Doctors_page_alter_info.php
@@ -47,14 +47,15 @@ transition: all 0.3s ease 0s;
 <div class="container"> 
   <!-- Navigation -->
   <header class="page_header"> 
-    <a href="Doctors_home_page.php"><span></span><h4 class="logo">DiaBeatIt</h4></span></a>
+    <a href="Home_login.php"><span></span><h4 class="logo">DiaBeatIt</h4></span></a>
   </a>
     <nav>
       <ul>
-        <li><a href="Doctors_home_page.php">Home</a></li>
+        <li><a href="Home_login.php">Home</a></li>
         <li><a href="Doctors_page.php">My page</a></li>
+		<li><a href="educational_page_login.php">Learn more</a></li>
 		<li><a href="logout.php">Logout</a></li>
-		<li style="color:yellow;font-weight:strong">Welcome, &nbsp;<br><?php echo $_SESSION['username']; ?></li>
+        <li style="color:yellow;font-weight:strong">Welcome, &nbsp;<br><?php echo $_SESSION['username']; ?></li>
         <!--<li> <a href="login.html">Sign In&nbsp;</a></li> -->
       </ul>
     </nav>
@@ -75,49 +76,17 @@ transition: all 0.3s ease 0s;
     	<strong>First name:</strong> <input type="text" name="fname" value='<?php echo $row_doctor[7];?>' required><br>
     	<strong>Last name: </strong><input type="text" name="lname" value='<?php echo $row_doctor[8];?>' required><br>
     	<strong>Workplace: </strong><input type="text" name="workplace" value='<?php echo $row_doctor[9];?>' required><br>
-    	<strong>SSN: </strong><input type="text" name="ssn" value='<?php echo $row_doctor[4];?>' required><br>
-    	<strong>Username: </strong><input type="text" name="username" value='<?php echo $row_doctor[1];?>' required><br>
-    	<strong>Email: </strong><input type="text" name="email" value='<?php echo $row_doctor[2];?>' required><br>
-    	<strong>Password: </strong><input type="text" name="pw1" placeholder="Enter password" required><br>
-    	<strong>Confirm password: </strong><input type="text" name="pw2" placeholder="Enter password" required><br>
+    	<strong>SSN: </strong><input type="tel" pattern="[0-9]{6}-[0-9]{4}" name="ssn" value='<?php echo $row_doctor[4];?>' required><br>
+    	<strong>Username: </strong><input type="text" maxlength = "16" name="username" value='<?php echo $row_doctor[1];?>' required><br>
+    	<strong>Email: </strong><input type="email" name="email" value='<?php echo $row_doctor[2];?>' required><br>
+    	<strong>Password: </strong><input type="password" maxlength = "50"  name="pw1" placeholder="Enter password" required><br>
+    	<strong>Confirm password: </strong><input type="password" maxlength = "50" name="pw2" placeholder="Confirm password" required><br>
     	<input type="submit" value="Save">
     	<button type="button" onClick="window.location.href = '../php/doctors_page.php';">Go back</button>
 	</form>
   </section>
 
 
-  <section class="text_column">
-    <h1 class = "nutritional_h1">Patients</h1>
-    <p><em>Summary of your patients</em>
-    <?php
-    include 'db.php';
-    $doctor_id = $_SESSION['id'];
-    $fetch_patients = "SELECT * FROM $dbname.patients_doctors WHERE $dbname.patients_doctors.doctor_id = '$doctor_id'";
-    $result = mysqli_query($link, $fetch_patients);
-
-    while($row = mysqli_fetch_row($result)) {
-        $fetch_info = "SELECT * FROM $dbname.users WHERE $dbname.users.id = '$row[0]'";
-        $result2 = mysqli_query($link, $fetch_info);
-        $row2 = mysqli_fetch_row($result2);
-        echo nl2br("".PHP_EOL."");
-        echo '<button id="btn'.$row[0].'" style="position:relative;left:5%">'.$row[0].'</button>';
-    }
-
-    ?>
-
-    <script>
-    var url = window.location.href;
-    $('#btn'.$row[0]).click(function($row[0]){
-        window.location.href="https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(window.location.href)+"&t="+document.title;
-    //$("#text_column").append(patient_page(count));
-    });
-    $('#btn2').attr("onclick", "patient_page(2)");
-    function patient_page(id) {
-       window.location.href="https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(window.location.href)+"&t="+document.title;
-    }
-
-
-    </script>
 
 </div>
 </body>
