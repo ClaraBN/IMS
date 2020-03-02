308cf0ccdcedb9605a8edad527143fe22263783f
Cris <cristinadiazubieta@gmail.com>
2020-03-02 13:13:45 +0100

    change sign in to log in in after_submit.php

--- HTML,CSS,JS/php/nutrition_after_submit.php
+++ HTML,CSS,JS/php/nutrition_after_submit.php
@@ -66,7 +66,7 @@ th {
         <li><a href="../html/nutrition.html">Nutrition checker</a></li>
 		<li><a href="../html/exercise.html">Calorie burn</a></li>
 		<li><a href="../html/educational_page.html">Learn more</a></li>
-        <li> <a href="../html/login.html">Sign In&nbsp;</a></li>
+        <li> <a href="../html/login.html">Log In&nbsp;</a></li>
       </ul>
     </nav>
   </header>
