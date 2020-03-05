efd8367b88b567cca6e0e36cebc396c5c41b61c7
Karl Svärd <karl.svard970@gmail.com>
2020-03-05 13:47:47 +0100

    Added header and footer to My health page after submitting

--- HTML,CSS,JS/php/My_health_after_submit.php
+++ HTML,CSS,JS/php/My_health_after_submit.php
@@ -1,4 +1,7 @@
 <!doctype html>
+<?php
+session_start(); // Right at the top of your script
+?>
 <html lang="en-US">
 <head>
 <meta charset="utf-8">
@@ -36,16 +39,7 @@ background: none;
 -moz-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
 transition: all 0.3s ease 0s;
 }
-.container_this {
-	width: 90%;
-	margin-left: auto;
-	margin-right: auto;
-	background-color: #FFFFFF;
-}
-.page_header{
-	height: 50px;
-	background-color: #73716A;
-}
+
 table, th, td {
   border: 1px solid black;
   border-collapse: collapse;
@@ -81,27 +75,31 @@ if(!isset($_SESSION['username'])){
 </head>
 <body>
 <!-- Main Container -->
-<div class="container_this">
+<div class="container">
   <!-- Navigation -->
-  <header class="page_header">
+  <header class="header"> 
     <a href="Home_login.php"><span></span><h4 class="logo">DiaBeatIt</h4></span></a>
-  </a>
-    <nav>
-      <ul>
-        <li><a href="Home_login.php">Home</a></li>
-        <li><a href="My_health.php">My health</a></li>
-        <li><a href="../html/exercise.html">Exercise tracker</a></li>
-		<li><a href="educational_page_login.php">Learn more</a></li>
-		<li><a href="logout.php">Logout</a></li>
-        <li style="color:yellow;font-weight:strong">Welcome, &nbsp;<br><?php echo $_SESSION['username']; ?></li>
+ 
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+      <ul class="menu">
+        <li><a href="../html/Home_login.php">Home</a></li>
+        <li><a href="../html/My_health.php">My health</a></li>
+        <li><a href="../html/exercise_login.php">Exercise tracker</a></li>
+	    <li><a href="../html/educational_page_login.php">Learn more</a></li>
+	    <li><a href="../html/logout.php">Logout</a></li>
+	    <li style="color:yellow;font-weight: bold;
+            background-color: #73716A; padding-top: 6px">
+            Welcome, &nbsp;<br><?php echo $_SESSION['username']; ?></li>
         <!--<li> <a href="login.html">Sign In&nbsp;</a></li> -->
       </ul>
-    </nav>
+    
   </header>
 
+  <section class="about" id="about" style="padding-top: 50px; padding-bottom: 450px;">
   <section class="text_column">
     <h1 class = "nutritional_h1">My status</h1>
-<div class="button_cont" ><a class="example_e" href="My_status.php" rel="nofollow noopener">Check my status</a></div>
+    <div class="button_cont" style="padding-top: 50px; padding-bottom: 25px;"><a class="example_e" href="My_status.php" rel="nofollow noopener">Check my status</a></div>
 	<h1 class = "nutritional_h1">Let us know what you ate</h1>
 	<div class="python_search_form">
 	<p style="font-weight: bold;color:black;">Enter the food item here, if you cannot find in our database</p>
@@ -176,4 +174,111 @@ function addNewRow(count){
 		<input type="submit" value="Submit and save">
 		</div>
 	</form>
+<br>
+<br>
+<?php
+//session_start // Right at the top of your script
+include 'db.php';
+$fat = 0;
+$Carbohydrates = 0;
+$Sugars = 0;
+$Fiber= 0;
+$Protein= 0;
+$Alcohol = 0;
+echo "<br>";
+$date = $link -> real_escape_string($_POST['date']);
+$time = $link -> real_escape_string($_POST['time']);
+$quantities = $_POST['Quantity'];
+$foods = $_POST['food_name'];
+
+$i = 0;
+foreach( $foods as  $food ) {
+	$quantity = $link -> real_escape_string($quantities[$i]);
+	$food = $link -> real_escape_string($food);
+	$i++;
+	
+	$food = preg_replace( "/, $/", "", $food ); 
+	$sql = "SELECT * FROM food WHERE name = '$food'";
+	$result_query = mysqli_query($link, $sql);
+
+	if (mysqli_num_rows($result_query) == 0) {
+		print("No such item in database<br>\n");
+	}
+	else {
+		print("<p style='color:black; font-weight: bold;'>Contents consumed from  '$food'  in grams</p>");
+		echo "<table border='1'>"; //define an html table
+		//<th> Defines a header cell in a table //<tr> Defines a row in a table
+		//<td> Defines a cell in a table
+		echo "<tr><th>Name</th><th>Fat</th><th>Carbohydrates</th><th>Sugars</th><th>Fiber</th><th>Protein</th><th>Alcohol</th></tr>";
+		while($row = mysqli_fetch_row($result_query)){
+		$foodid = $row[0];
+		echo "<tr><td>";
+		echo $row[1];
+		echo "</td><td>";
+		echo $row[2] * $quantity;
+		echo "</td><td>";
+		echo $row[3] * $quantity;
+		echo "</td><td>";
+		echo $row[4] * $quantity;
+		echo "</td><td>";
+		echo $row[5] * $quantity;
+		echo "</td><td>";
+		echo $row[6] * $quantity;
+		echo "</td><td>";
+		echo $row[7] * $quantity;
+		echo "</td></tr>";
+		$fat = $fat + ($row[2] * $quantity);
+		$Carbohydrates = $Carbohydrates + ($row[3] * $quantity);
+		$Sugars = $Sugars + ($row[4] * $quantity);
+		$Fiber= $Fiber + ($row[5] * $quantity);
+		$Protein= $Protein + ($row[6] * $quantity);
+		$Alcohol = $Alcohol + ($row[7] * $quantity);
+	}
+		echo "</table>";
+		echo "<br>";
+	}
+
+	$insert = "INSERT INTO diet VALUES('$date','$time','$foodid','".$_SESSION['id']."','$quantity')";
+	mysqli_query($link, $insert) ;
+}
+		echo ("<p style='color:black; font-weight: bold;'>Total amount of contents consumed in grams</p>");
+		echo "<table border='1'>"; //define an html table
+		//<th> Defines a header cell in a table //<tr> Defines a row in a table
+		//<td> Defines a cell in a table
+		echo "<tr><th>Name</th><th>Fat</th><th>Carbohydrates</th><th>Sugars</th><th>Fiber</th><th>Protein</th><th>Alcohol</th></tr>";
+		echo "<tr><td>";
+		echo "Total content";
+		echo "</td><td>";
+		echo $fat;
+		echo "</td><td>";
+		echo $Carbohydrates;
+		echo "</td><td>";
+		echo $Sugars;
+		echo "</td><td>";
+		echo $Fiber;
+		echo "</td><td>";
+		echo $Protein;
+		echo "</td><td>";
+		echo $Alcohol;
+		echo "</td></tr>";
+		echo "</table>";
+		echo "<br>";
+?>
 </section>
+  </section>
+  <section> 
+  <h1 class="hero_header">
+  <footer class="footer">
+    <p style="margin-top: 0px;margin-bottom: 0px; text-align: center">Contact us! <br/>
+    <a href="http://facebook.com"><img src="../images/facebook_icon.png" alt="facebook icon" height="30" width="30" /></a> &nbsp
+    <a href="http://gmail.com"><img src="../images/gmail_icon.png" alt="gmail icon" height="30" width="30" /></a> &nbsp
+    <a href="http://instagram.com"><img src="../images/instagram_icon.png" alt="instagram icon" height="30" width="30" /></a> &nbsp
+    <a href="http://linkedin.com"><img src="../images/linkedin_icon.png" alt="linkedin icon" height="30" width="30" /></a> &nbsp
+    <a href="http://twitter.com"><img src="../images/twitter_icon.png" alt="twitter icon" height="30" width="30" /></a> &nbsp
+    </p>
+  </footer>
+  </h1> 
+  </section> 
+</div>
+</body>
+</html> 
