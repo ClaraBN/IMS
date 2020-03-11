4e40b6881afa949f2810b309bdf7ec73ae2e8c0f
Clara Bernedal Nordstroem <clara.nordstrom@live.se>
2020-03-11 10:53:37 +0100

    fixed broken path apparently in the two register files

--- HTML,CSS,JS/php/register_user.php
+++ HTML,CSS,JS/php/register_user.php
@@ -217,7 +217,7 @@ function test_input($data) {
             <ul class="menu">
                 <li><a href="../html/Home.html">Home</a></li>
                 <li><a href="../html/nutrition.html">Nutrition checker</a></li>
-		<li><a href="..html/exercise.html">Exercise tracker</a></li>
+		<li><a href="../html/exercise.html">Exercise tracker</a></li>
                 <li><a href="../html/educational_page.html">Learn more</a></li>
                 <li> <a href="../html/login.html">Log In&nbsp;</a></li>
             </ul>
